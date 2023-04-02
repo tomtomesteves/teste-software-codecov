55891478c68fba8b0a417eff1fee8e6cc26ad8cf
Thomas Esteves <thomasesteves95@hotmail.com>
2023-04-02 13:17:23 -0300

    [test] update test

--- test_main.py
+++ test_main.py
@@ -3,5 +3,8 @@ from main import custom_sum
 
 
 class test_main(unittest.TestCase):
-    def test_sum(self):
+    def test_sum_positive(self):
         self.assertEqual(custom_sum(2, 3), 5)
+
+    def test_sum_negative(self):
+        self.assertEqual(custom_sum(-2, 3), 1)
