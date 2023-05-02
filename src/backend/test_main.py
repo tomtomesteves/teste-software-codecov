5e09ed6838ea99d506886d7a5160337b8f88f672
Thomas Esteves <thomasesteves95@hotmail.com>
2023-05-02 13:53:13 +0000

    test/custom_div tests

--- src/backend/test_main.py
+++ src/backend/test_main.py
@@ -1,5 +1,5 @@
 import unittest
-from main import custom_sum
+from main import custom_sum, custom_div
 
 
 class test_main(unittest.TestCase):
@@ -8,3 +8,10 @@ class test_main(unittest.TestCase):
 
     def test_sum_negative(self):
         self.assertEqual(custom_sum(-2, 3), 1)
+
+    def test_div(self):
+        self.assertEqual(custom_div(3, 2), 1.5)
+
+    def test_div_exception(self):
+        with self.assertRaises(ZeroDivisionError):
+            custom_div(2, 0)
