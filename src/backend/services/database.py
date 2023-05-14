7dc039d1429cd046d7dc3b55a80c5a424e1cc6a5
Thomas Esteves <thomasesteves95@hotmail.com>
2023-05-14 11:37:45 -0300

    Fix database retry and implements testes for missing lines

--- src/backend/services/database.py
+++ src/backend/services/database.py
@@ -1,4 +1,7 @@
 import psycopg2
+import time
+
+MAX_RETRY = 10
 
 class PostgreSQLConnection:
     def __init__(self, host, port, database, user, password):
@@ -11,14 +14,25 @@ class PostgreSQLConnection:
         self.cursor = None
 
     def connect(self):
-        self.connection = psycopg2.connect(
-            host=self.host,
-            port=self.port,
-            database=self.database,
-            user=self.user,
-            password=self.password
-        )
-        self.cursor = self.connection.cursor()
+        retry = 0
+        while retry < MAX_RETRY:
+            try:
+                self.connection = psycopg2.connect(
+                    host=self.host,
+                    port=self.port,
+                    database=self.database,
+                    user=self.user,
+                    password=self.password
+                )
+            except psycopg2.OperationalError:
+                print(f"Error connecting to DB, retry number {retry}")
+                time.sleep(5)
+            retry += 1
+        
+        if self.connection:
+            self.cursor = self.connection.cursor()
+        else:
+            raise psycopg2.OperationalError
 
     def close(self):
         if self.cursor:
