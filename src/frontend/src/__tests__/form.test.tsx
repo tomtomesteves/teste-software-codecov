b52e5bb2306949ee04e7fe4ae0f2a4650bd8e5a9
Evaldo Martins de Souza <evaldo.ms@hotmail.com>
2023-05-20 17:51:48 -0300

    feat: adding some frontend tests

--- src/frontend/src/__tests__/form.test.tsx
+++ src/frontend/src/__tests__/form.test.tsx
@@ -67,4 +67,130 @@ describe('ItemForm', () => {
     )
     expect((getByTestId('complete-control') as any).checked).toBe(true)
   })
+
+  it('submit button is enabled when form is filled correctly', () => {
+    const { getByTestId } = render(<ItemForm handleOnSubmit={noop} />);
+  
+    fireEvent.change(getByTestId('title-control'), {
+      target: { value: 'Tarefa 1' },
+    });
+    fireEvent.change(getByTestId('description-control'), {
+      target: { value: 'Descrição da tarefa' },
+    });
+  
+    expect(getByTestId('submit-btn')).not.toBeDisabled();
+  })
+
+  it('title field is required', () => {
+    const { getByTestId, getByText } = render(<ItemForm handleOnSubmit={noop} />);
+    
+    fireEvent.click(getByTestId('submit-btn'));
+    
+    expect(getByText('Title is required')).toBeInTheDocument();
+  })
+
+  it('description field is required', () => {
+    const { getByTestId, getByText } = render(<ItemForm handleOnSubmit={noop} />);
+    
+    fireEvent.change(getByTestId('title-control'), {
+      target: { value: 'Tarefa 1' },
+    });
+    
+    fireEvent.click(getByTestId('submit-btn'));
+    
+    expect(getByText('Description is required')).toBeInTheDocument();
+  })
+
+  it('form is reset after submission', () => {
+    const { getByTestId } = render(<ItemForm handleOnSubmit={noop} />);
+    
+    fireEvent.change(getByTestId('title-control'), {
+      target: { value: 'Tarefa 1' },
+    });
+    
+    fireEvent.change(getByTestId('description-control'), {
+      target: { value: 'Descrição da tarefa' },
+    });
+    
+    fireEvent.click(getByTestId('submit-btn'));
+    
+    expect((getByTestId('title-control') as any).value).toBe('');
+    expect((getByTestId('description-control') as any).value).toBe('');
+  })
+  
+  it('displays success message after form submission', () => {
+    const { getByTestId, getByText } = render(<ItemForm handleOnSubmit={noop} />);
+    
+    fireEvent.change(getByTestId('title-control'), {
+      target: { value: 'Tarefa 1' },
+    });
+    
+    fireEvent.change(getByTestId('description-control'), {
+      target: { value: 'Descrição da tarefa' },
+    });
+    
+    fireEvent.click(getByTestId('submit-btn'));
+    
+    expect(getByText('Form submitted successfully')).toBeInTheDocument();
+  })
+  
+  it('displays error message on invalid form submission', () => {
+    const { getByTestId, getByText } = render(<ItemForm handleOnSubmit={noop} />);
+    
+    fireEvent.click(getByTestId('submit-btn'));
+    
+    expect(getByText('Error: Form submission failed')).toBeInTheDocument();
+  })
+  
+  it('title field supports a maximum of 50 characters', () => {
+    const { getByTestId, getByText } = render(<ItemForm handleOnSubmit={noop} />);
+    
+    fireEvent.change(getByTestId('title-control'), {
+      target: { value: 'This is a very long title that exceeds the character limit' },
+    });
+    
+    expect(getByText('Title cannot exceed 50 characters')).toBeInTheDocument();
+  })
+  
+  it('description field supports a maximum of 200 characters', () => {
+    const { getByTestId, getByText } = render(<ItemForm handleOnSubmit={noop} />);
+    
+    fireEvent.change(getByTestId('description-control'), {
+      target: { value: 'This is a very long description that exceeds the character limit. It is used for testing purposes.' },
+    });
+    
+    expect(getByText('Description cannot exceed 200 characters')).toBeInTheDocument();
+  })
+  
+  it('handleOnSubmit is called with correct form values', () => {
+    const handleOnSubmit = jest.fn();
+    const { getByTestId } = render(<ItemForm handleOnSubmit={handleOnSubmit} />);
+    
+    fireEvent.change(getByTestId('title-control'), {
+      target: { value: 'Tarefa 1' },
+    });
+    
+    fireEvent.change(getByTestId('description-control'), {
+      target: { value: 'Descrição da tarefa' },
+    });
+    
+    fireEvent.click(getByTestId('submit-btn'));
+    
+    expect(handleOnSubmit).toHaveBeenCalledWith({
+      title: 'Tarefa 1',
+      description: 'Descrição da tarefa',
+    });
+  })  
+
+  it('handleOnSubmit is not called when form is empty', () => {
+    const handleOnSubmit = jest.fn();
+    const { getByTestId } = render(<ItemForm handleOnSubmit={handleOnSubmit} />);
+    
+    fireEvent.click(getByTestId('submit-btn'));
+    
+    expect(handleOnSubmit).not.toHaveBeenCalled();
+  })
+  
+  
+  
 })
